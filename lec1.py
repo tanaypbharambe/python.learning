9d187760674022dce2be41b97225b97ffd8f5029
tanaypbharambe <tanaygaming99@gmail.com>
2025-06-20 23:35:11 +0530

    Add files via upload
    this was my first lecture and i am so curious about second lecture because i learnt a cool thing and i made a project to calculate a average of two numbers

---
+++ lec1.py
@@ -0,0 +1,10 @@
+#i learnt variables , operations , input and output commands and types such as "str","int","float"
+# str is for english alphabates with no numbers , int (integers), float (ex.3.57)
+#i learnt about >= and <= which is lessthan equal to and greater than equal to 
+# on these things i done 5 questions by my self 
+#1 calculate average of 2 numbers 
+a=int(input("enter number 1"))
+b=int(input("enter number 2 "))
+average=(a+b)/2 #basic maths
+print("the average of the two numbers is :",average)
+# this was one of my 5 questions
